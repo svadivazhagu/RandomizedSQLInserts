433c1a6e1a6d5d0547e236437ddc45eacaac749d
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2020-02-04 22:45:49 -0500

    Initial commit

---
+++ InsertIntoPatients.py
@@ -0,0 +1,40 @@
+import faker
+from random import randint
+
+
+def printPatient(numPatients):
+    for i in range(numPatients):
+        person = faker.Faker()
+        ssn = "'" + str(randint(100000000, 999999999)) + "'"
+        fname = "'" + person.name().split(' ')[0] + "'"
+        lname = "'" + person.name().split(' ')[1] + "'"
+        addy = "'" + str(person.address()[:randint(20, 35)]) + "'"
+        telnum = "'" + str(randint(100000000, 9999999999)) + "'"
+
+        print('INSERT INTO Patient'
+              + '('
+              + 'SSN'
+              + ','
+              + 'FirstName'
+              + ','
+              + 'LastName'
+              + ','
+              + 'Address'
+              + ','
+              + 'TelNum'
+              + ') '
+              + 'VALUES'
+              + '('
+              + ssn
+              + ','
+              + fname
+              + ','
+              + lname
+              + ','
+              + addy
+              + ','
+              + telnum
+              + ');')
+
+
+
