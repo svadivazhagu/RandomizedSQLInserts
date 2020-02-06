be5af8154cb1dfe5d7f950440dfa380debf372ea
svadivazhagu <svadivazhagu@wpi.edu>
2020-02-05 19:48:44 -0500

    finished phase 1, need to do phase 2. automated most of phase 3

--- InsertIntoPatients.py
+++ InsertIntoPatients.py
@@ -2,7 +2,7 @@
 #code to create strings to insert random data for X amount of fake patients
 
 import faker
-from random import randint
+from random import randint, choice
 
 
 def printPatient(numPatients):
@@ -37,6 +37,76 @@ def printPatient(numPatients):
               + addy
               + ','
               + telnum
+              + ');')
+
+
+def printDoctors(numDoctors):
+    for i in range(numDoctors):
+        person = faker.Faker()
+        id = "'" + str(randint(10000, 99999)) + "'"
+        fname = "'" + person.name().split(' ')[0] + "'"
+        lname = "'" + person.name().split(' ')[1] + "'"
+        gender = choice(['M', 'F'])
+        speciality = choice(['General', 'Eye', 'Nose', 'Mouth', 'Hair', 'Lips', 'Back', 'Lungs', 'Brain', 'Chest', 'Legs'])
+        print('INSERT INTO Doctor'
+              + '('
+              + 'Id'
+              + ','
+              + 'FirstName'
+              + ','
+              + 'LastName'
+              + ','
+              + 'Gender'
+              + ','
+              + 'Specialty'
+              + ') '
+              + 'VALUES'
+              + '('
+              + id
+              + ','
+              + fname
+              + ','
+              + lname
+              + ','
+              + gender
+              + ','
+              + speciality
+              + ');')
+
+def printRooms(numRooms):
+    for i in range(numRooms):
+        room_num = "'" + str(randint(100, 999)) + "'"
+        occupied = "'" + str(randint(0, 1)) + "'"
+        print('INSERT INTO Room'
+            + '('
+            + 'Num'
+            + ','
+            + 'Occupied'
+            + ') '
+            + 'VALUES'
+            + '('
+            + room_num
+            + ','
+            + occupied
+            + ');')
+
+
+
+def printRoomService(numRoomService):
+    for i in range(numRoomService):
+        room_num = "'" + str(randint(100, 999)) + "'"
+        service =  "'" + choice(['Emergency', 'Maternity', 'Nursing', 'Surgery', 'Pharmacy']) + "'"
+        print('INSERT INTO RoomService'
+              + '('
+              + 'RoomNum'
+              + ','
+              + 'Service'
+              + ') '
+              + 'VALUES'
+              + '('
+              + room_num
+              + ','
+              + service
               + ');')
 
 
